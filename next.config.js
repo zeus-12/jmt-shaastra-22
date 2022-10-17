51ee05a9e0717828aa966ba2ebeb1c35d238d25d
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-10-17 22:24:03 +0530

    hope this works

--- next.config.js
+++ next.config.js
@@ -1,19 +1,40 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
 
-// module.exports = nextConfig
+// module.exports = {
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       config.resolve.fallback.fs = false;
+//       config.resolve.fallback.tls = false;
+//       config.resolve.fallback.net = false;
+//       config.resolve.fallback.child_process = false;
+//     }
+
+//     return config;
+//   },
+// };
 module.exports = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-      config.resolve.fallback.tls = false;
-      config.resolve.fallback.net = false;
-      config.resolve.fallback.child_process = false;
-    }
+  // other next config
+  i18n: {
+    locales: ["en", "ms"],
+    defaultLocale: "en",
+  },
+  env: {
+     SPREADSHEET_ID : ***,
+     SHEET_ID : ***,
+     CLIENT_EMAIL : ***,
+     PRIVATE_KEY : ***,
 
-    return config;
+  },
+  webpack: (config, options) => {
+    config.node= {
+    // Some libraries import Node modules but don't use them in the browser.
+    // Tell Webpack to provide empty mocks for them so importing them works.
+    ...config.node,
+    fs: 'empty',
+    child_process : 'empty',
+    net : 'empty',
+    tls: 'empty',
+  }
+
+    return config
   },
 };
