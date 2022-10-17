c572aaca5446f1eaf899cd8fee629216aaef30aa
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-10-17 22:24:08 +0530

    ^

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-
 // module.exports = {
 //   webpack: (config, { isServer }) => {
 //     if (!isServer) {
@@ -17,24 +16,18 @@ module.exports = {
     locales: ["en", "ms"],
     defaultLocale: "en",
   },
-  env: {
-     SPREADSHEET_ID : ***,
-     SHEET_ID : ***,
-     CLIENT_EMAIL : ***,
-     PRIVATE_KEY : ***,
 
-  },
   webpack: (config, options) => {
-    config.node= {
-    // Some libraries import Node modules but don't use them in the browser.
-    // Tell Webpack to provide empty mocks for them so importing them works.
-    ...config.node,
-    fs: 'empty',
-    child_process : 'empty',
-    net : 'empty',
-    tls: 'empty',
-  }
+    config.node = {
+      // Some libraries import Node modules but don't use them in the browser.
+      // Tell Webpack to provide empty mocks for them so importing them works.
+      ...config.node,
+      fs: "empty",
+      child_process: "empty",
+      net: "empty",
+      tls: "empty",
+    };
 
-    return config
+    return config;
   },
 };
