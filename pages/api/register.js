afb60b2aed2aa59bea66606cb55b01a7f3f73b73
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-10-17 23:10:29 +0530

    sheeeeesh

--- pages/api/register.js
+++ pages/api/register.js
@@ -146,9 +146,9 @@ async function handler(req, res) {
       },
     });
 
-    res.status(201).json({ message: "It works!", response });
+    res.status(200).json({ success: "success" });
+    return;
   }
-  res.status(200).json({ message: "Hey!" });
 }
 
 export default handler;
